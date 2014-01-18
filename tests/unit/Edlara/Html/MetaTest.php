9fdcd84b8272d0912a7a25ab8bbb53b5170a493f
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2014-01-18 15:14:17 +0530

    Update:
    MetaData-gen:
    Added more tests dealing with events and improved existing events
    
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- tests/unit/Edlara/Html/MetaTest.php
+++ tests/unit/Edlara/Html/MetaTest.php
@@ -6,9 +6,11 @@ class MetaTest extends TestCase {
 
     public function after(){
         m::close();
+        unset($this->mock);
     }
 
     public function testConstruct(){
+
         $this->mock =  m::mock('Illuminate\Html\HtmlBuilder');
         $this->mock->shouldReceive('macro')->once()->andReturn(null);
         $this->html = new Edlara\Html\Meta($this->mock);
@@ -24,4 +26,34 @@ class MetaTest extends TestCase {
     }
 
 
+    public function testSetGetAuthorWithEvent(){
+
+        Event::listen('meta.author',function(){
+            return "Testing";
+        });
+
+        $this->mock = new Edlara\Html\Meta();
+        $this->mock->setAuthor("Bar");
+        $this->assertNotEquals($this->mock->getAuthor(),"Bar");
+
+    }
+
+    public function testSetGetCharset(){
+
+        $this->mock = new Edlara\Html\Meta();
+        $this->mock->setCharset("Western");
+        $this->assertEquals("Western",$this->mock->getCharset());
+
+    }
+
+    public function testSetGetCharsetWithEvent(){
+
+        Event::listen("meta.charset",function(){
+            return "Unicode";
+        });
+
+        $this->mock = new Edlara\Html\Meta();
+        $this->mock->setCharset("US-ENC");
+        $this->assertNotEquals("US-ENC",$this->mock->getCharset());
+    }
 }
