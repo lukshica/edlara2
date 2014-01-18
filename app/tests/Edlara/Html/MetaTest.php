ffda9ffdf3589b78516d2bed87e4c84e83a91410
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2014-01-18 15:20:04 +0530

    Update:
    PHPUnit Based Tests, copied from codeception dir.
    
    currently maintaining two branches of tests to have distinct phpunit
    and codeception tests.
    
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- app/tests/Edlara/Html/MetaTest.php
+++ app/tests/Edlara/Html/MetaTest.php
@@ -1,10 +1,59 @@
 <?php
 
-use Mockery\Mockery;
+use \Mockery as m;
 
 class MetaTest extends TestCase {
-    public function testSetAuthor(){
-        $this->assertTrue(true);
+
+    public function after(){
+        m::close();
+        unset($this->mock);
     }
 
+    public function testConstruct(){
+
+        $this->mock =  m::mock('Illuminate\Html\HtmlBuilder');
+        $this->mock->shouldReceive('macro')->once()->andReturn(null);
+        $this->html = new Edlara\Html\Meta($this->mock);
+
+    }
+
+    public function testSetGetAuthor(){
+
+        $this->mock = new Edlara\Html\Meta();
+        $this->mock->setAuthor('Testing Author');
+        $this->assertEquals($this->mock->getAuthor(),"Testing Author");
+
+    }
+
+
+    public function testSetGetAuthorWithEvent(){
+
+        Event::listen('meta.author',function(){
+            return "Testing";
+        });
+
+        $this->mock = new Edlara\Html\Meta();
+        $this->mock->setAuthor("Bar");
+        $this->assertNotEquals($this->mock->getAuthor(),"Bar");
+
+    }
+
+    public function testSetGetCharset(){
+
+        $this->mock = new Edlara\Html\Meta();
+        $this->mock->setCharset("Western");
+        $this->assertEquals("Western",$this->mock->getCharset());
+
+    }
+
+    public function testSetGetCharsetWithEvent(){
+
+        Event::listen("meta.charset",function(){
+            return "Unicode";
+        });
+
+        $this->mock = new Edlara\Html\Meta();
+        $this->mock->setCharset("US-ENC");
+        $this->assertNotEquals("US-ENC",$this->mock->getCharset());
+    }
 }
