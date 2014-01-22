e17e1ad694d01b83a23dcc4f2125ba188ffc6274
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2014-01-22 07:47:17 +0530

    Update:
    Renamed View Files and added some code.
    
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- public/themes/default/packages/orchestra/foundation/layout/main.blade.php
+++ public/themes/default/packages/orchestra/foundation/layout/main.blade.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
-	<head>
-		@include('orchestra/foundation::layout._header')
-	</head>
-	<body>
-		@include('orchestra/foundation::layout._navigation')
-		<? Orchestra\Support\Facades\Site::set('header::class', 'main-header') ?>
-		@include('orchestra/foundation::components.header')
-		<section class="container main">
-			@include('orchestra/foundation::components.messages')
-			@yield('content')
-		</section>
-		@include('orchestra/foundation::layout._footer')
-	</body>
+{{--The HTML Tag Specified Here make useof the Laravel Config--}}
+<html lang="{{ Config::get('app.locale') }}">
+    <head>
+        @include('orchestra/foundation::layout.head')
+    </head>
+    <body>
+        @include('orchestra/foundation::layout.nav')
+        <? Orchestra\Support\Facades\Site::set('header::class', 'main-header') ?>
+        @include('orchestra/foundation::components.header')
+        <section class="container main">
+            @include('orchestra/foundation::components.messages')
+            @yield('content')
+        </section>
+        @include('orchestra/foundation::layout._footer')
+
+        <!-- Livereload Script for Development Purposes -->
+        @if(app()->env=="local")
+        <script src="http://localhost:35729/livereload.js"></script>
+        @endif
+    </body>
 </html>
