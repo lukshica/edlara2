f563adaa206d0caf3700a0c014daa5912023286c
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2014-01-25 20:17:42 +0530

    Update:
    Updated Main Template File
    
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- public/themes/default/layout/main.blade.php
+++ public/themes/default/layout/main.blade.php
@@ -1,13 +1,20 @@
 <!doctype html>
 <html lang="{{ Config::get('application.language') }}">
-<head>
-    {{Meta::charset()}}
-    {{HTML::title()}}
-    {{Meta::author()}}
-    {{Meta::description()}}
-    {{Meta::generator()}}
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
-</body>
+    <head>
+        {{Meta::charset()}}
+
+        {{HTML::title()}}
+
+        {{OG::all()}}
+
+        {{Meta::author()}}
+
+        {{Meta::description()}}
+
+        {{Meta::generator()}}
+
+        {{Meta::viewport()}}
+    </head>
+    <body>
+    </body>
 </html>
