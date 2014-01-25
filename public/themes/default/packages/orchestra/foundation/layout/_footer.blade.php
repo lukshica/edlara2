30fd166a25962de32dd7ce417d3ec00195494729
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2014-01-25 20:23:52 +0530

    Update:
    Removed:
        * Deleted Miniprofile
        * Updated View Files.
    
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- public/themes/default/packages/orchestra/foundation/layout/_footer.blade.php
+++ public/themes/default/packages/orchestra/foundation/layout/_footer.blade.php
@@ -6,30 +6,16 @@ use Orchestra\Support\Facades\Asset; ?>
 	<div class="container">
 		<hr>
 		<p>
-			Powered by <a href="http://orchestraplatform.com" target="_blank">Orchestra Platform</a>.
+            Proudly Powered by <a href="//github.com/grans/edlara">Edlara</a>. Educational Laravel Package
 		</p>
 	</div>
 </footer>
 
-<?
-
+<?php
 $asset = Asset::container('orchestra/foundation::footer');
-
-$asset->script('bootstrap', 'packages/orchestra/foundation/vendor/bootstrap/js/bootstrap.min.js');
-$asset->script('jquery-ui', 'packages/orchestra/foundation/vendor/jquery.ui/jquery.ui.js');
-$asset->script('orchestra', 'packages/orchestra/foundation/js/orchestra.min.js', array('bootstrap', 'jquery-ui'));
-$asset->script('jui-toggleSwitch', 'packages/orchestra/foundation/vendor/delta/js/jquery-ui.toggleSwitch.js', array('jquery-ui'));
-$asset->script('select2', 'packages/orchestra/foundation/vendor/select2/select2.min.js'); ?>
+?>
 
 {{ $asset->styles() }}
 {{ $asset->scripts() }}
-
 @placeholder("orchestra.layout: footer")
 
-<script>
-jQuery(function onPageReady($) { 'use strict';
-	var dispatcher = Javie.make('event');
-
-	dispatcher.fire("orchestra.ready: <?php echo Request::path(); ?>");
-});
-</script>
--- public/themes/default/packages/orchestra/foundation/layout/main.blade.php
+++ public/themes/default/packages/orchestra/foundation/layout/main.blade.php
@@ -1,21 +1,29 @@
 <!DOCTYPE html>
-{{--The HTML Tag Specified Here make useof the Laravel Config--}}
+{{--The HTML Tag Specified Here make use of the Laravel Config--}}
 <html lang="{{ Config::get('app.locale') }}">
     <head>
+        {{--Head Layout--}}
         @include('orchestra/foundation::layout.head')
     </head>
     <body>
+        {{--Navigation Bar Layout--}}
         @include('orchestra/foundation::layout.nav')
-        <? Orchestra\Support\Facades\Site::set('header::class', 'main-header') ?>
+
+        {{--Header Layout--}}
         @include('orchestra/foundation::components.header')
+
+        {{--Main Container--}}
         <section class="container main">
             @include('orchestra/foundation::components.messages')
             @yield('content')
         </section>
+
+        {{--Footer--}}
         @include('orchestra/foundation::layout._footer')
 
-        <!-- Livereload Script for Development Purposes -->
+        {{--Developer Option--}}
         @if(app()->env=="local")
+        {{--Livereload Script for Development Purposes--}}
         <script src="http://localhost:35729/livereload.js"></script>
         @endif
     </body>
