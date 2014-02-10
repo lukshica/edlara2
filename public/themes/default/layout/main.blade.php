184ba7342f3dc51b3629fd6c13b1e95ab201d02f
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2014-02-10 19:33:43 +0530

    Update: 	* Updated Main Template file to include compiled and minified 		css build file.
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- public/themes/default/layout/main.blade.php
+++ public/themes/default/layout/main.blade.php
@@ -14,7 +14,16 @@
         {{Meta::generator()}}
 
         {{Meta::viewport()}}
+        <link rel="stylesheet" type="text/css" href="/builds/frontend/style.min.css"/>
     </head>
     <body>
+
+
+
+    {{--Developer Option--}}
+    @if(app()->env=="local")
+    {{--Livereload Script for Development Purposes--}}
+    <script src="http://localhost:35729/livereload.js"></script>
+    @endif
     </body>
 </html>
