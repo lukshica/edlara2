c123d121a3888ec5b46ac775ef64468e5b60ca22
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2014-02-10 06:10:20 +0530

    Add:
    	WolframAlpha API building began.
    
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

---
+++ app/library/Edlara/Quest/Resolver/WolframAlpha/Bot.php
@@ -0,0 +1,11 @@
+<?php namespace Edlara\Quest\Resolver\WolframAlpha;
+
+
+class Bot
+{
+    public function __construct()
+    {
+
+    }
+
+} 
---
+++ app/library/Edlara/Quest/Resolver/WolframAlpha/Engine/Pod.php
@@ -0,0 +1,11 @@
+<?php namespace Edlara\Quest\Resolver\WolframAlpha\Engine;
+
+
+class Pod
+{
+    public function __construct()
+    {
+
+    }
+
+}
